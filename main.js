2d476f4f781207aed1558bf00997f17e4c080eb8
Mallarie <mallariemilano@gmail.com>
2019-01-17 21:05:28 -0800

    structured Madlib and added input and id's

---
+++ main.js
@@ -0,0 +1,17 @@
+// first input
+var firstNounInput = document.getElementById('noun-1');
+
+// second input
+var adjectiveInput = document.getElementById('adjective');
+
+// third input
+var verbInput = document.getElementById('verb');
+
+// fourth input
+var bodyPartInput = document.getElementById('bodyPart');
+
+// fifth input
+var secondNounInput = document.getElementById('noun-2');
+
+// sixth input
+var adverbInput = document.getElementById('adverb');
