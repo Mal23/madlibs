03858bf4aa86a5ff81b389fc982a9d2b8ded13d1
Mallarie <mallariemilano@gmail.com>
2019-01-18 16:45:15 -0800

    structured js doc and html connection

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-// first input
-var firstNounInput = document.getElementById('noun-1');
+function story() {
 
-// second input
+// inputs by id
+var firstNounInput = document.getElementById('noun-one');
 var adjectiveInput = document.getElementById('adjective');
-
-// third input
 var verbInput = document.getElementById('verb');
-
-// fourth input
-var bodyPartInput = document.getElementById('bodyPart');
-
-// fifth input
-var secondNounInput = document.getElementById('noun-2');
-
-// sixth input
+var bodyPartInput = document.getElementById('body-part');
+var secondNounInput = document.getElementById('noun-two');
 var adverbInput = document.getElementById('adverb');
+
+// get values out of inputs
+var nounOne = nounOneInput.value;
+var adjective = adjectiveInput.value;
+var verb = verbInput.value;
+var bodyPart = bodyPartInput.value;
+var nounTwo = nounTwoInput.value;
+var adverb = adverbInput.value;
+
+// make the story
+var story = 'Please add one' + nounOne + ' ' + 'to your' + adjective + ' ' + 'with your' + bodyPart + ' ' + 'season with' + nounTwo + '. ' + 'Then enjoy' + adverb + ' ! ' + 'Ah-ah-ah';
+console.log(story);
+
+// display the story
+var madLib = document.getElementById('madLib');
+console.log(madLib);
+madLib.textContent = story;
+}
