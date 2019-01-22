914eb27397807d359342b1f853ad34e7b7e61ed0
Mallarie <mallariemilano@gmail.com>
2019-01-22 11:45:05 -0800

    changed verb to adverb

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function story() {
 // inputs by id
 var nounOneInput = document.getElementById('noun-one');
 var adjectiveInput = document.getElementById('adjective');
-var verbInput = document.getElementById('verb');
+var adverbOneInput = document.getElementById('adverb-one');
 var bodyPartInput = document.getElementById('body-part');
 var nounTwoInput = document.getElementById('noun-two');
 var adverbInput = document.getElementById('adverb');
@@ -11,13 +11,13 @@ var adverbInput = document.getElementById('adverb');
 // get values out of inputs
 var nounOne = nounOneInput.value;
 var adjective = adjectiveInput.value;
-var verb = verbInput.value;
+var adverbOne = adverbOneInput.value;
 var bodyPart = bodyPartInput.value;
 var nounTwo = nounTwoInput.value;
 var adverb = adverbInput.value;
 
 // make the story
-var story = 'Please add one' + ' ' + nounOne + ' ' + 'to your' +  ' ' + adjective + " " + 'stew. ' + 'Stir' + ' ' + verb + ' ' + 'with your' + ' ' + bodyPart  + '. ' + ' ' + 'Season with' + ' ' + nounTwo + '. ' + ' '  + 'Then enjoy' + ' ' + adverb + '! ' + ' ' + 'Ah-ah-ah';
+var story = 'Please add one' + ' ' + nounOne + ' ' + 'to your' +  ' ' + adjective + " " + 'stew. ' + 'Stir' + ' ' + adverbOne + ' ' + 'with your' + ' ' + bodyPart  + '. ' + ' ' + 'Season with' + ' ' + nounTwo + '. ' + ' '  + 'Then enjoy' + ' ' + adverb + '! ' + ' ' + 'Ah-ah-ah';
 console.log(story);
 
 // display the story
